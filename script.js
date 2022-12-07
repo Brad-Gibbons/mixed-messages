005aa4c24787d6768a7b7448b236cb4a2152d2b2
Brad Gibbons <bradgibbonsdev@gmail.com>
2022-12-07 14:00:29 +0000

    add functionality

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ let destination = ['egg', 'pond', 'station', 'tour de france', 'smoothie'];
 
 console.log(item, destination)
 
+// Create prototype for random item
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
+  }
+
 // Function to print random message 
+const printMessage = () => {
+    return `Why did the ${item.random()} cross the road? To get to the ${destination.random()}`
+}
+
+console.log(printMessage());
