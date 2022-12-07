c2006625b95894556c5804150e5778e7aef49430
Brad Gibbons <bradgibbonsdev@gmail.com>
2022-12-07 13:55:26 +0000

    add script file

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// Define date
+let item = ['chicken', 'frog', 'train', 'cyclist', 'orange'];
+let destination = ['egg', 'pond', 'station', 'tour de france', 'smoothie'];
+
+console.log(item, destination)
+
+// Function to print random message 
